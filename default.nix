6da2af5b21bb71c26d51f2a48bd40030f49c141c
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-08 14:27:49 +0300

    Use php53Packages from overlay

--- default.nix
+++ default.nix
@@ -121,45 +121,7 @@ sh = dash.overrideAttrs (_: rec {
   postInstall = ''(cd "$out/include" && ln -s ImageMagick* ImageMagick)'';
  };
 
- buildPhp53Package = args: buildPhpPackage ({ php = php53; } // args);
-
-
-php53Packages = {
-  timezonedb = buildPhp53Package {
-    name = "timezonedb";
-    version = "2019.1";
-    sha256 = "0rrxfs5izdmimww1w9khzs9vcmgi1l90wni9ypqdyk773cxsn725";
-  };
-
-  dbase = buildPhp53Package {
-      name = "dbase";
-      version = "5.1.0";
-      sha256 = "15vs527kkdfp119gbhgahzdcww9ds093bi9ya1ps1r7gn87s9mi0";
-  };
-
-  intl = buildPhp53Package {
-      name = "intl";
-      version = "3.0.0";
-      sha256 = "11sz4mx56pc1k7llgbbpz2i6ls73zcxxdwa1d0jl20ybixqxmgc8";
-      inputs = [ icu58 ];
-  };
-
-  zendopcache = buildPhp53Package {
-      name = "zendopcache";
-      version = "7.0.5";
-      sha256 = "1h79x7n5pylbc08cxl44fvbi1a1592n0w0mm847jirkqrhxs5r68";
-  };
-
-  imagick = buildPhp53Package {
-      name = "imagick";
-      version = "3.1.2";
-      sha256 = "528769ac304a0bbe9a248811325042188c9d16e06de16f111fee317c85a36c93";
-      inputs = [ pkgconfig imagemagick68 pcre ];
-      configureFlags = [ "--with-imagick=${imagemagick68}" ];
-  };
-};
-
-  rootfs = mkRootfs {
+   rootfs = mkRootfs {
       name = "apache2-php53-rootfs";
       src = ./rootfs;
       inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd mjHttpErrorPages php53 postfix s6 execline zendguard connectorc mjperl5Packages ;
