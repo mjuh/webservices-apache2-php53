84842603dbf78c8e9c33256f5a8185908e5c63e1
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-11-20 13:17:53 +0000

    parser3

--- default.nix
+++ default.nix
@@ -50,6 +50,8 @@ pkgs.dockerTools.buildLayeredImage rec {
     (optipng.override{ inherit libpng ;})
     gifsicle nss-certs.unbundled zip
     perl
+    gcc-unwrapped.lib
+    glibc
   ]
   ++ collect isDerivation phpDeprecatedPackages.php53Packages
   ++ collect isDerivation mjperl5Packages;
